2bb46b7aeee5412ac02238b9996bfe0cc44434df
batti251 <bastibuenz25@gmail.com>
2025-10-07 07:05:07 +0200

    fix: remove html-buttons from canvas when game starts

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ function toggleLegalNotice(){
 }
 
 function startGame() {
+    removeButtons();
     world = new World(canvas, keyboard, 0);
+}
+
+function removeButtons(){
+    let buttons = document.getElementsByTagName('button');
+    [...buttons].forEach(btn => btn.classList.toggle('d-none'))
 }
